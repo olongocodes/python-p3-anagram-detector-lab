d8fc0cf06ab181a1f752ba827be2053962f7487d
Kevin <kevinolongo9@gmail.com>
2023-11-29 11:39:45 +0300

    sort anagram

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,7 @@
 # your code goes here!
+class Anagram:
+    def __init__(self,word):
+        self.word = word
+        
+    def match(self, possible_anagrams):
+        return [anagram for anagram in possible_anagrams if sorted(anagram) == sorted(self.word)]
